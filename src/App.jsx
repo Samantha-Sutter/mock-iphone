c090dc7b82271cc923b275c789deb51f1e5ca30f
Samantha Sutter <sutters4@gmatc.matc.edu>
2024-07-10 10:19:37 -0500

    Iphone Update

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar";
+import Highlights from "./components/Highlights";
+import Hero from "./components/Hero";
+import Model from "./components/Model";
+
+import * as Sentry from '@sentry/react';
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
+  )
+}
+
+export default Sentry.withProfiler(App);
+
